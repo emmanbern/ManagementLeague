3f88d5bbf9e4d62955e8c055e727da7ea3a1029a
emmanbern <emmanbern1@gmail.com>
2018-03-14 00:18:05 -0400

    Configuration du webapi

---
+++ LeagueAppManagement/Areas/Admins/Api/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+
+namespace LeagueAppManagement.Areas.Admins.Api
+{
+    public class PlayerController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult GetValue()
+        {
+            return Ok(new { info = "Test" });
+        }
+
+        [HttpPost]
+        public IHttpActionResult GetOtherValue()
+        {
+            return Ok(new { info = "Test2" });
+        }
+
+        public IHttpActionResult CreatePlayer()
+        {
+            return Ok();
+        }
+    }
+}
--- LeagueAppManagement/Areas/Admins/Scripts/editController.js
+++ LeagueAppManagement/Areas/Admins/Scripts/editController.js
@@ -1,14 +1,21 @@
-﻿leagueApp.controller('editController', ['$scope', function ($scope) {
+﻿leagueApp.controller('editController', ['$scope', '$http', function ($scope, $http) {
 
     $scope.Init = function () {
         $scope.Test = {
             Nom: "Toto"
         };
         console.log($scope);
+        $http.post('http://league.local/Api/Player/GetValue', {}).then(function (response) {
+            console.log(response);
+        });
+        $http.post('http://league.local/Api/Player/GetOtherValue', {
+
+        }).then(function (response) {
+            console.log(response);
+        });
     };
 
     $scope.Log = function (value) {
-        console.log(value);
-        console.log($scope);
+       
     };
 }]);
