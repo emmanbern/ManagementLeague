f9abde401d5b52581233918a6816709cacec0781
emmanbern <emmanbern1@gmail.com>
2018-03-21 20:45:48 -0400

    Ajout du service d'énumeration générique

---
+++ LeagueAppManagement/Services/EnumerationService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeagueAppManagement.Services
+{
+    public class EnumerationService
+    {
+        public List<string> GetStringsFromEnumeration<T>() where T : struct
+        {
+            var enumStrings = new List<string>();
+            Type genericType = typeof(T);
+            if (genericType.IsEnum)
+            {
+                foreach (T obj in Enum.GetValues(genericType))
+                {
+                    enumStrings.Add(obj.ToString());
+                }
+            }
+            return enumStrings;
+        }
+
+        public List<KeyValuePair<string, int>> GetEnumerationValues<T>() where T : struct
+        {
+            var enumValues = new List<KeyValuePair<string, int>>();
+            Type genericType = typeof(T);
+            if (genericType.IsEnum)
+            {
+                foreach (T obj in Enum.GetValues(genericType))
+                {
+                    Enum intValue = Enum.Parse(typeof(T), obj.ToString()) as Enum;
+                    int number = Convert.ToInt32(intValue);
+
+                    enumValues.Add(new KeyValuePair<string, int>(obj.ToString(), number));
+                }
+            }
+            return enumValues;
+        }
+    }
+}
---
+++ LeagueAppManagementTest/Services/EnumerationServiceTest.cs
@@ -0,0 +1,39 @@
+﻿using LeagueAppManagement.Data;
+using LeagueAppManagement.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace LeagueAppManagementTest.Services
+{
+    [TestClass]
+    public class EnumerationServiceTest
+    {
+
+        private EnumerationService _service;
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _service = new EnumerationService();
+        }
+
+        [TestMethod]
+        public void Test_GetAllEnumString()
+        {
+            var result = _service.GetStringsFromEnumeration<GradeEnum>();
+
+            Assert.AreEqual("A", result.First());
+            Assert.AreEqual("B", result.ElementAt(1));
+            Assert.AreEqual("C", result.Last());
+        }
+
+        [TestMethod]
+        public void Test_GetEnumerationValues()
+        {
+            var result = _service.GetEnumerationValues<GradeEnum>();
+
+            Assert.AreEqual(3, result.Count);
+            Assert.AreEqual("A", result.First().Key);
+            Assert.AreEqual(1, result.First().Value);
+        }
+    }
+}
