64a053b039db2eb25e5f991848f9fac19b75c002
emmanbern <emmanbern1@gmail.com>
2018-03-24 13:23:01 -0400

    Création du controller Team

---
+++ LeagueAppManagement/Areas/Admins/Controllers/ManageTeamController.cs
@@ -0,0 +1,32 @@
+﻿using LeagueAppManagement.Areas.Admins.Models;
+using LeagueAppManagement.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace LeagueAppManagement.Areas.Admins.Controllers
+{
+    public class ManageTeamController : Controller
+    {
+        ApplicationDbContext _dbContext;
+
+        public ManageTeamController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        public ActionResult List()
+        {
+            var players = _dbContext.Players.Select(s => new PlayerViewModel
+            {
+                FirstName = s.FirstName,
+                LastName = s.LastName,
+                GradeEnum = s.Grade,
+                PhoneNumber = s.PhoneNumber,
+                Guid = s.Guid,
+                PositionEnum = s.Position
+            }).ToList();
+
+            return View(players);
+        }
+
+    }
+}
